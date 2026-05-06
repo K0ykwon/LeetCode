09ecb9a26abeb68feb7071481b7bb3c96fa140d0
YoungKwon Ko <yko081524@yonsei.ac.kr>
2026-05-06 20:03:23 +0900

    Time: 155 ms (64.28%), Space: 19.3 MB (22.37%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.py
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.py
@@ -6,8 +6,8 @@ class Solution(object):
         :rtype: int
         """
         res = 0
-        for i in range(5,n+1):
-            for j in range(3,i):
+        for i in range(1,n+1):
+            for j in range(1,i):
                 c = i**2 - j**2
                 if math.isqrt(c)**2 == c: res += 1
         return res
